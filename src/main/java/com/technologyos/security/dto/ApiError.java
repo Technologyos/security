6536fba54fef73e60d5d46df4d0f3cab078a4eed
ArmandoSJ <just-jauregui@hotmail.com>
2025-05-18 21:26:19 -0600

    add api error class

---
+++ src/main/java/com/technologyos/security/dto/ApiError.java
@@ -0,0 +1,23 @@
+package com.technologyos.security.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiError implements Serializable {
+   private String backendMessage;
+   private String message;
+   private String url;
+   private String method;
+   @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+   private LocalDateTime timestamp;
+}
