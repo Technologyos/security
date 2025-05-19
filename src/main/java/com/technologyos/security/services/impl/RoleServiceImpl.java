8896bc4ccdb5d2bc078b85f759654ef4b2ab90fd
ArmandoSJ <just-jauregui@hotmail.com>
2025-05-19 16:19:34 -0600

    fix problem with the security default role value

--- src/main/java/com/technologyos/security/services/impl/RoleServiceImpl.java
+++ src/main/java/com/technologyos/security/services/impl/RoleServiceImpl.java
@@ -3,16 +3,16 @@ package com.technologyos.security.services.impl;
 import com.technologyos.security.entities.Role;
 import com.technologyos.security.repositories.RoleRepository;
 import com.technologyos.security.services.RoleService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
 public class RoleServiceImpl implements RoleService {
-   private final RoleRepository roleRepository;
+   @Autowired
+   private RoleRepository roleRepository;
 
    @Value("${security.default.role}")
    private String defaultRole;
