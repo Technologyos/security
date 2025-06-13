26ca098bc73bf8d1237b9d6f501320f0dafc0d3d
ArmandoSJ <just-jauregui@hotmail.com>
2025-06-13 14:00:43 -0600

    add initial status repository

---
+++ src/main/java/com/technologyos/auth/repositories/StatusRepository.java
@@ -0,0 +1,6 @@
+package com.technologyos.auth.repositories;
+
+import com.technologyos.auth.entities.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatusRepository extends JpaRepository<Status, Long> {}
