a7d08d263714fc5532e50af6e59b038ebea68e1e
ArmandoSJ <just-jauregui@hotmail.com>
2025-06-13 13:48:29 -0600

    update api exception, add documentation and code field

--- src/main/java/com/technologyos/auth/dtos/ApiException.java
+++ src/main/java/com/technologyos/auth/dtos/ApiException.java
@@ -1,6 +1,7 @@
 package com.technologyos.auth.dtos;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ApiError implements Serializable {
+public class ApiException implements Serializable {
+   @Schema(description = "The unique error code", name = "code", example = "200")
+   private int code;
    private String backendMessage;
+   @Schema(description = "A human-readable explanation of the error", name = "message",
+      example = "The user does not have the properly permissions to access the "
+         + "resource, please contact with ")
    private String message;
    private String url;
    private String method;
