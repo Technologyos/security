5e4e98aca323e7e903d5d6be0b41f95f59243d70
ArmandoSJ <just-jauregui@hotmail.com>
2025-06-20 11:03:16 -0600

    add role request

---
+++ src/main/java/com/technologyos/auth/dtos/role/RoleRequest.java
@@ -0,0 +1,18 @@
+package com.technologyos.auth.dtos.role;
+
+import com.technologyos.auth.dtos.status.StatusEnum;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleRequest {
+   @NotBlank
+   private String name;
+   private StatusEnum statusId;
+}
