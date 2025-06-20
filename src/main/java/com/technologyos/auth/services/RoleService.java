c73597b4bb218a4c317bb49a7a36b5ee45b4258f
ArmandoSJ <just-jauregui@hotmail.com>
2025-06-20 12:55:05 -0600

    add initial role service implementation

--- src/main/java/com/technologyos/auth/services/RoleService.java
+++ src/main/java/com/technologyos/auth/services/RoleService.java
@@ -1,9 +1,26 @@
 package com.technologyos.auth.services;
 
+import com.technologyos.auth.dtos.role.RoleRequest;
 import com.technologyos.auth.entities.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface RoleService {
-   Optional<Role> findDefaultRole();
+   Role findDefaultRole();
+
+   Page<Role> findAll(Pageable pageable);
+
+   Role findRoleById(Long roleId);
+
+   Role createRole(RoleRequest roleRequest);
+
+   Role updateRole(RoleRequest roleRequest, Long roleId);
+
+   Role disableById(Long roleId);
+
+   Role enableById(Long roleId);
+
+   List<Role> findByStatusId(Long statusId);
 }
