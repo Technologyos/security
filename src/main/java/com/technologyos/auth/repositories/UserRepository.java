4d3c85f4a85afd589ad2e30cbab363e73d240e4d
ArmandoSJ <just-jauregui@hotmail.com>
2025-06-22 22:22:00 -0600

    add findByStatusId operation in UserRepository

--- src/main/java/com/technologyos/auth/repositories/UserRepository.java
+++ src/main/java/com/technologyos/auth/repositories/UserRepository.java
@@ -3,10 +3,13 @@ package com.technologyos.auth.repositories;
 import com.technologyos.auth.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
    Optional<User> findByUsername(String username);
 
    Optional<User> findByEmail(String email);
+
+   List<User> findByStatusId(Long statusId);
 }
